ab885086b97a93335a5d35db7db027b83a5752c3
JaacielBriseo <jaacielbris23@gmail.com>
2023-01-22 23:15:24 -0700

    Interfaces for products

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 function App() {
 	return (
 		<div className='App'>
-			<h1 className="font-Manrope font-bold">App</h1>
+			<h1 className='font-Manrope font-bold'>App</h1>
+			<img src='./assets/product-yx1-earphones/mobile/image-product.jpg' alt='' />
 		</div>
 	);
 }
