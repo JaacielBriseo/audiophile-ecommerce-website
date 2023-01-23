ac9259f0abc19ff727bca3a6e6daaa8a7f58f5f9
JaacielBriseo <jaacielbris23@gmail.com>
2023-01-22 23:50:35 -0700

    React router initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,5 @@
-import { useContext } from "react";
-import { ProductsContext } from "./context";
+import AppRouter from './router/AppRouter';
 
-function App() {
-	const {products} = useContext(ProductsContext)
-	return (
-		<div className='App'>
-			<h1 className='font-Manrope font-bold'>App</h1>
-			<img src='./assets/product-yx1-earphones/mobile/image-product.jpg' alt='' />
-		</div>
-	);
-}
+const App = () => <AppRouter />;
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 import { ProductsProvider } from './context';
+import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
-		<ProductsProvider>
-			<App />
-		</ProductsProvider>
+		<BrowserRouter>
+			<ProductsProvider>
+				<App />
+			</ProductsProvider>
+		</BrowserRouter>
 	</React.StrictMode>
 );
