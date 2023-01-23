5d12f098779ea5ffd912a890483eede5d6281b78
JaacielBriseo <jaacielbris23@gmail.com>
2023-01-22 21:53:08 -0700

    Initial setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+	return (
+		<div className='App'>
+			<h1 className="font-Manrope font-bold">App</h1>
+		</div>
+	);
+}
+
+export default App;
---
+++ tailwind.config.cjs
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				Manrope: ['Manrope', 'sans-serif'],
+			},
+			colors: {
+				BurntOrange: '#D87D4A',
+				Dark: '#101010',
+				VeryLightGrey: '#F1F1F1',
+				WhiteSmoke: '#FAFAFA',
+				LightBeige: '#FBAF85',
+				White: '#FFFFFF',
+				VeryBlack: '#000000',
+			},
+		},
+	},
+	plugins: [],
+};
